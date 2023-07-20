e486ad69e0ba418cfdcf546adaf92e470e0f03a8
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-21 00:18:37 +0100

    added some code for DHT

--- myDHT22.py
+++ myDHT22.py
@@ -3,18 +3,30 @@ import time
 import json
 from datetime import datetime
 
-DHT_SENSOR = Adafruit_DHT.DHT22
-DHT_PIN = 4
 
-class Sensor(object):
-	"""docstring foSensorme"""
-	def __init__(self, arg):
-		superSensore, self).__init__()
-		self.arg = arg
 
+class DHT(object):
+	def __init__(self, pin, model):
+		self.pin = pin
+		self.model = model
+
+	def get_pin(self):
+		return self.pin
+	
+	def get_model(self):
+		return self.model
+	
+	def get_results(self): #This method returns a tuple which contains the (humidity, temp) readings
+		return Adafruit_DHT.read_retry(self.model, self.pin)
+
+
+
+
+'''
+sensor = DHT(4, Adafruit_DHT.DHT22)	
 
 while True:
-	humidity, temperature = Adafruit_DHT.read_retry(DHT_SENSOR, DHT_PIN)
+	humidity, temperature = sensor.get_results()
 	timestamp = datetime.now()
 	if humidity is not None and temperature is not None:
 		#print("Temp = {0:0.1f}*C Humidity = {1:0.1f}%".format(temperature, humidity))
@@ -24,4 +36,4 @@ while True:
 	else:
 		print("Sensor failure. Check wiring.")
 		time.sleep(3)
-
+'''
