1c1fe562c37ad6ff497e9335e3bd70ba84e92a2e
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-26 01:32:37 +0100

    update

--- Program_files/plant_monitor_test.py
+++ Program_files/plant_monitor_test.py
@@ -4,9 +4,9 @@ from Sensors_devices.crops import Plant
 import time
 
 raspberry_pi = plant_monitor("RaspberryPi", "connection_string", 4, "DHT22", 0)
-raspberry_pi.add_plant("Lettuce", 18.0, 50.0, 70, 1, 4672, 11568)
-raspberry_pi.add_plant("Lettuce", 18.0, 50.0, 70, 2, 6192, 13648)
-raspberry_pi.add_plant("Lettuce", 18.0, 50.0, 70, 3, 7904, 14048)
+raspberry_pi.add_plant("Lettuce", "Lettuce", 18.0, 50.0, 70, 1, 4672, 11568)
+raspberry_pi.add_plant("Lettuce", "Lettuce", 18.0, 50.0, 70, 2, 6192, 13648)
+raspberry_pi.add_plant("Lettuce", "Lettuce", 18.0, 50.0, 70, 3, 7904, 14048)
 
 
 while True:
