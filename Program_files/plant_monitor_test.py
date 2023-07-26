60cce11db3242ef3ad3eb02c9ece1594ff1cc9cb
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-26 17:33:30 +0100

    sorted csv files

--- Program_files/plant_monitor_test.py
+++ Program_files/plant_monitor_test.py
@@ -3,11 +3,19 @@ from Sensors_devices.crops import Plant
 
 import time
 
-raspberry_pi = plant_monitor("RaspberryPi", "connection_string", 4, "DHT22", 0)
-raspberry_pi.add_plant("Lettuce1", "Lettuce", 18.0, 50.0, 70, 1, 4672, 11568)
-raspberry_pi.add_plant("Lettuce2", "Lettuce", 18.0, 50.0, 70, 2, 6192, 13648)
-raspberry_pi.add_plant("Lettuce3", "Lettuce", 18.0, 50.0, 70, 3, 7904, 14048)
+dry_sensor1 = 8814
+dry_sensor2 = 8754
+dry_sensor3 = 13008
 
+wet_sensor1 = 6418
+wet_sensor2 = 6232
+wet_sensor3 = 9427
+
+raspberry_pi = plant_monitor("RaspberryPi", "connection_string", 4, "DHT22", 0)
+raspberry_pi.add_plant("Lettuce1", "Lettuce", 18.0, 50.0, 70, 1, wet_sensor1, dry_sensor1)
+raspberry_pi.add_plant("Lettuce2", "Lettuce", 18.0, 50.0, 70, 2, wet_sensor2, dry_sensor2)
+raspberry_pi.add_plant("Lettuce3", "Lettuce", 18.0, 50.0, 70, 3, wet_sensor3, dry_sensor3)
+ 
 
 while True:
     print(raspberry_pi.get_results())
