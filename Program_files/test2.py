5023052e408db74fb93a888a4713af5ad532cb72
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-26 12:32:42 +0100

    code

--- Program_files/test2.py
+++ Program_files/test2.py
@@ -1,8 +1,17 @@
 from Sensors_devices.analog_calib import analogue_ada_sensor
 from Sensors_devices.analog_calib import get_control_value, mapValue
+import time
 
 light_sensor = analogue_ada_sensor("Light intensity", 0)
 
-readings = get_control_value(light_sensor, 10)
+#readings = get_control_value(light_sensor, 10)
 
-print("The reading is: " + str(readings))
+#print("The reading is: " + str(readings))
+
+
+max_value = 23344
+min_value = 6816
+
+while True:
+    print(str(light_sensor.get_mapped_value(min_value, max_value)) + "%")
+    time.sleep(1)
