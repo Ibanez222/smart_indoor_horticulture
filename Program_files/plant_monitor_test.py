20d0c0dd0239f26b808a9a6e3c7dc4dd1cafc7ce
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-27 17:48:21 +0100

    test file is now updated

--- Program_files/plant_monitor_test.py
+++ Program_files/plant_monitor_test.py
@@ -3,19 +3,18 @@ from Sensors_devices.crops import Plant
 
 import time
 
-dry_sensor1 = 8814
-dry_sensor2 = 8754
-dry_sensor3 = 13008
+dry_sensor1_reading = 15968
+wet_sensor1_reading = 8432
+dry_sensor2_reading = 15636
+wet_sensor2_reading = 8416
+dry_sensor3_reading = 17536
+wet_sensor3_reading = 12776
 
-wet_sensor1 = 6418
-wet_sensor2 = 6232
-wet_sensor3 = 9427
+raspberry_pi = plant_monitor("RaspberryPi", "", 4, "DHT22", 0)
+raspberry_pi.add_plant("Lettuce1", "Lettuce", 18.0, 75.0, 70, 1, wet_sensor1_reading, dry_sensor1_reading)
+raspberry_pi.add_plant("Lettuce2", "Lettuce", 18.0, 75.0, 70, 2, wet_sensor3_reading, dry_sensor2_reading)
+raspberry_pi.add_plant("Lettuce3", "Lettuce", 18.0, 75.0, 70, 3, wet_sensor3_reading, dry_sensor3_reading)
 
-raspberry_pi = plant_monitor("RaspberryPi", "connection_string", 4, "DHT22", 0)
-raspberry_pi.add_plant("Lettuce1", "Lettuce", 18.0, 50.0, 70, 1, wet_sensor1, dry_sensor1)
-raspberry_pi.add_plant("Lettuce2", "Lettuce", 18.0, 50.0, 70, 2, wet_sensor2, dry_sensor2)
-raspberry_pi.add_plant("Lettuce3", "Lettuce", 18.0, 50.0, 70, 3, wet_sensor3, dry_sensor3)
- 
 
 while True:
     print(raspberry_pi.get_results())
