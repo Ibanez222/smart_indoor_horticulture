7e525c6110a5cbc8c4e711f23d428760ea42a504
Ibanez222 <p.panlilio@rgu.ac.uk>
2023-07-31 23:29:51 +0100

    changed the time to 5 minutes sending data

--- Program_files/send_telemetry2.py
+++ Program_files/send_telemetry2.py
@@ -47,7 +47,7 @@ def iothub_client_telemetry_sample_run():
             print( "Sending message: {}".format(message) )
             client.send_message(message)
             print ( "Message successfully sent" )
-            time.sleep(500)
+            time.sleep(300)
 
     except KeyboardInterrupt:
         print ( "IoTHubClient sample stopped" )
